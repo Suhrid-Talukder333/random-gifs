5f54b7676ef307b13d16a6aa916d72f4261d0ccb
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-03-15 19:53:03 +0600

    started

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+const App = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Suhrid
+          </a>
+        </div>
+      </nav>
+        <div className="fullpage row">
+            <div className="container"></div>
+        </div>
+    </>
+  );
+};
+
+export default App;
