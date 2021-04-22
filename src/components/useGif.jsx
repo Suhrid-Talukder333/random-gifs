67eb92450a88abaa7d466b33056d9114e1401f83
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-04-23 00:27:25 +0600

    done apu key hiding

--- src/components/useGif.jsx
+++ src/components/useGif.jsx
@@ -1,21 +1,21 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const useGif = (tag) => {
-    const [gif, setGif] = useState("");
+  const [gif, setGif] = useState("");
 
-    const fetchData = async (tag) => {
-      const url = `https://api.giphy.com/v1/gifs/random?api_key=rn4GM2Rjqp764mcfiaYNWIiYfE2oAltM&tag=${tag}&rating=r`;
-      const { data } = await axios.get(url);
-      const src = data.data.images.downsized_medium.url;
-      setGif(src);
-    };
+  const fetchData = async (tag) => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.REACT_APP_API_KEY}&tag=${tag}&rating=r`;
+    const { data } = await axios.get(url);
+    const src = data.data.images.downsized_medium.url;
+    setGif(src);
+  };
 
-    useEffect(() => {
-        fetchData(tag);
-    }, [tag]);
+  useEffect(() => {
+    fetchData(tag);
+  }, [tag]);
 
-    return { gif, fetchData };
-}
+  return { gif, fetchData };
+};
 
 export default useGif;
